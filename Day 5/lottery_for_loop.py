1cbbc8505a879bb09d226af09b4c2425ec6cc863
Truston Ailende <trustonailende@gmail.com>
2021-07-05 20:33:13 +0100

    Added lottery file
    Modified the Lottery Simulator to use a WHILE and a FOR loop.

---
+++ Day 5/lottery_for_loop.py
@@ -0,0 +1,19 @@
+from random import randint
+
+# Generate a number from 1 to 36
+number = randint(1, 36)
+
+# Define an empty list
+numbers = []
+
+# Create a generator
+while len(numbers) < 6:
+    if number not in numbers:
+        numbers.append(number)
+    else:
+        pass
+    number = randint(1, 36)
+
+# Print the elements of the list
+for number in numbers:
+    print(number)
